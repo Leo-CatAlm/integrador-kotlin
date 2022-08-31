d0a835c4dbb8abad7e957a1c21bf77b93c819169
lcatejo <leonardo.catejo@mercadolibre.cl>
2022-08-31 10:33:02 -0400

    Add parkedTime val

--- app/src/main/java/com/example/integradorkotlin/ParkingSpace.kt
+++ app/src/main/java/com/example/integradorkotlin/ParkingSpace.kt
@@ -1,12 +1,16 @@
 package com.example.integradorkotlin
 
-data class ParkingSpace(var plate : Vehicle) {
+import java.util.*
+
+const val MINUTES_IN_MILLISECONDS = 60000
+
+data class ParkingSpace(var vehicle : Vehicle) {
+
+    val parkingTime: Long
+        get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis)/MINUTES_IN_MILLISECONDS
 
     //BORRADOR
     //CHECKIN-CHECKOUT
     //PAYMENT
-  fun timeInParking(vehicle: Vehicle){
-
-  }
 
 }
