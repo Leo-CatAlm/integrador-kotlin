79f62f831a4b4c6421278ac2fe9200ee3bb78bdd
lcatejo <leonardo.catejo@mercadolibre.cl>
2022-08-31 17:19:46 -0400

    Add preset values to Parking

--- app/src/main/java/com/example/integradorkotlin/Parking.kt
+++ app/src/main/java/com/example/integradorkotlin/Parking.kt
@@ -1,5 +1,17 @@
 package com.example.integradorkotlin
 
-data class Parking(val vehicles : MutableSet<Vehicle>) {
+//Se definen valores de inicio para poder hacer más
+data class Parking(val vehicles: MutableSet<Vehicle> = mutableSetOf(), val maxCapacity: Int = 20) {
 
+    fun addVehicle(vehicle: Vehicle): Boolean {
+
+        return if (vehicles.size < maxCapacity) {
+            vehicles.add(vehicle)
+            println("Welcome to AlkeParking!")
+            true
+        } else {
+            println("Sorry, the check-in failed")
+            false
+        }
+    }
 }
